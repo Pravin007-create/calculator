97931adb2703c84c4ecfd3c09d5900c59f2683dc
Pravin007-create <79079282+Pravin007-create@users.noreply.github.com>
2024-05-20 14:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const display = document.querySelector(".display")
+
+function append(input) {
+    display.value += input
+}
+
+function cleardisplay() {
+    display.value = ''
+}
+function calculate() {
+    try{
+        display.value = eval(display.value)
+    }catch(error){
+        display.value ="Error";
+
+    }
+    
+        
+   
+}
+
